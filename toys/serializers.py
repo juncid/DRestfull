c0473dfca7ea772307465bd6a6d64e6ad6e4fdf0
juncid <juncid@gmail.com>
2020-02-18 12:23:30 -0300

    Fix: fix class ToySerializer

--- toys/serializers.py
+++ toys/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Toy
 
 
-class ToySerializer(serializers.Serializer):
+class ToySerializer(serializers.ModelSerializer):
     class Meta:
         model = Toy
         fields = ('id',
