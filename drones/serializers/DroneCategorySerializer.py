4d7b8055a5177bc28c320ebc16df510bacdfb17e
juncid <juncid@gmail.com>
2020-02-20 19:56:56 -0300

    FIX: fix view_name on DroneCategorySerializer

--- drones/serializers/DroneCategorySerializer.py
+++ drones/serializers/DroneCategorySerializer.py
@@ -7,7 +7,7 @@ class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
     drones = serializers.HyperlinkedRelatedField(
         many=True,
         read_only=True,
-        view_name='drone_detail'
+        view_name='drone-detail'
     )
 
     class Meta:
