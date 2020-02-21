b9fb51eeabca58ed78a68fcb21c1d7c9802f99eb
juncid <juncid@gmail.com>
2020-02-21 19:48:25 -0300

    SETTING: Configure filter backend classes

--- DRestfull/settings.py
+++ DRestfull/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'toys.apps.ToysConfig',
     'drones.apps.DronesConfig',
     'django_extensions',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -136,5 +137,10 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'drones.custompagination.LimitOffsetPaginationWithUpperBound',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 4,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter'
+    ),
 }
