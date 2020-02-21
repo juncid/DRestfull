077279ed1193ae987a188f1319dcc93dc3db4a49
juncid <juncid@gmail.com>
2020-02-20 21:14:45 -0300

    MIGRATION: Add unique for column name on Drone, DroneCategory and Pilot Models.

---
+++ drones/models/Drone.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from .DroneCategory import DroneCategory
+
+
+class Drone(models.Model):
+
+    name = models.CharField(max_length=250, unique=True)
+    drone_category = models.ForeignKey(
+        DroneCategory,
+        related_name='drones',
+        on_delete=models.CASCADE
+    )
+    manufacturing_date = models.DateTimeField()
+    has_it_competed = models.BooleanField(default=False)
+    inserted_timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
