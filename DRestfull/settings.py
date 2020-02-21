b69807d0c1e4dbbce560d96076ca8f1ea9821a01
juncid <juncid@gmail.com>
2020-02-21 18:38:56 -0300

    SETTING: Configure Limit Offset Pagination

--- DRestfull/settings.py
+++ DRestfull/settings.py
@@ -135,6 +135,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'drones.custompagination.LimitOffsetPaginationWithUpperBound',
     'PAGE_SIZE': 4
 }
