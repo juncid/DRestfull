01b1517e839d844859b636064c1e3e5851da61ea
juncid <juncid@gmail.com>
2020-02-25 21:17:59 -0300

    Test: add try to post pilot without token.

--- drones/tests.py
+++ drones/tests.py
@@ -162,3 +162,20 @@ class PilotTests(APITestCase):
         self.client.credentials()
         unauthorized_get_response = self.client.get(url, format='json')
         assert unauthorized_get_response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_try_to_post_pilot_without_token(self):
+        """
+        Ensure we cannot create a pilot without a token
+        """
+        new_pilot_name = 'Unauthorized Pilot'
+        new_pilot_gender = Pilot.FEMALE
+        new_pilot_races_count = 5
+        response = self.post_pilot(
+            new_pilot_name,
+            new_pilot_gender,
+            new_pilot_races_count
+        )
+        print(response)
+        print(Pilot.objects.count())
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert Pilot.objects.count() == 0
